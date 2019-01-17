74b0a9073d7b42a628f3f64cb4965aa917e30d5e
Kayla Roberts <klr3of8@gmail.com>
2019-01-17 10:39:46 -0700

    nav test

---
+++ web/w02-team/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+}
+
+switch ($action) {
+    case 'about':
+        $about = true;
+        
+        break;
+}
--- web/w02-team/login.php
+++ web/w02-team/login.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href='main.css'>
     <title>Document</title>
 </head>
 <body>
     <header>
-        <?php include 'header.php'; ?>
+        <?php $login = "active"; include 'header.php'; ?>
     </header>
     <main>
         <ul>
