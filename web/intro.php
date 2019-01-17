d3733127b6e843c6f80ba6683a3372aa3272b3d6
Kayla Roberts <klr3of8@gmail.com>
2019-01-16 22:48:50 -0700

    started adding basic files for assignment

--- web/intro.php
+++ web/intro.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="">
-    <title>Home</title>
+    <title>Document</title>
 </head>
 <body>
-    <a></a>
-    <a></a>
-    <footer></footer>
+    
 </body>
 </html>
