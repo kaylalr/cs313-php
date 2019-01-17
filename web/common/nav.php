a3c4fbcc8ad5be232c75d237ad90e7a874703a69
Kayla Roberts <klr3of8@gmail.com>
2019-01-17 11:50:19 -0700

    added home link

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,4 +1,5 @@
 <ul>
+    <li><a href='index.php'>Home</a></li>
     <li><a href="index.php?action=intro">Introduction</a></li>
     <li><a href="index.php?action=assignments">Assignments</a></li>
 </ul>
