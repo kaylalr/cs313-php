9cf90de18609fff0536cd0a90929c2426f88c0f1
Kayla Roberts <klr3of8@gmail.com>
2019-01-18 10:55:04 -0700

    added bootstrap

--- web/home.php
+++ web/home.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="main.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title>Home</title>
     </head>
     <body>
@@ -15,7 +17,7 @@
             <?php include 'common/nav.php' ?>
         </nav>
         <main>
-            <a href="intro.php">Introduction</a><br>
+            <a href="intro.php">Introduction</a><br><br>
             <a href="assignments.php">Assignments</a><br>
         </main>
         <footer>
