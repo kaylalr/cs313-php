abc23e8a8f02634e04a547641ac17053dd94182f
Kayla Roberts <klr3of8@gmail.com>
2019-01-22 11:22:21 -0700

    changed nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,4 +1,33 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=intro">Introduction</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=assignments">Assignments</a>
+      </li>
+<!--      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>-->
+    </ul>
+  </div>
+</nav>
+<!--<nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
@@ -7,7 +36,7 @@
       <a class="nav-item nav-link" href="index.php?action=assignments">Assignments</a>
     </div>
   </div>
-</nav>
+</nav>-->
 <!--<ul>
     <li><a href='index.php'>Home</a></li>
     <li><a href="index.php?action=intro">Introduction</a></li>
