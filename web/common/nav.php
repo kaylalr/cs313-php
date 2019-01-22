22590619239853b8abf7b7e436585781d9537b54
Kayla Roberts <klr3of8@gmail.com>
2019-01-22 11:09:43 -0700

    changed nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,14 +1,10 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Navbar</a>
-<!--  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>-->
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="index.php">Home</a>
+      <a class="nav-item nav-link active" href="index.php">Home</a>
       <a class="nav-item nav-link" href="index.php?action=intro">Introduction</a>
       <a class="nav-item nav-link" href="index.php?action=assignments">Assignments</a>
-      <!--<a class="nav-item nav-link disabled" href="#">Disabled</a>-->
     </div>
   </div>
 </nav>
