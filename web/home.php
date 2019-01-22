02d452dde0d92c4ac61e297ac59543eed2539946
Kayla Roberts <klr3of8@gmail.com>
2019-01-22 12:00:16 -0700

    navbar update

--- web/home.php
+++ web/home.php
@@ -5,17 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="main.css">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title>Home</title>
     </head>
     <body>
         <header>
             <?php include 'common/header.php' ?>
         </header>
-        <div class="container">
-            <?php include 'common/nav.php' ?>
-        </div>
+        <!--<div class="container">-->
+        <?php include 'common/nav.php' ?>
+        <!--</div>-->
         <main>
             <a href="intro.php">Introduction</a><br><br>
             <a href="assignments.php">Assignments</a><br>
