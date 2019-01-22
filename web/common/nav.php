ac9de92e1c25dfa1d84f31c27c384c426ccddeb0
Kayla Roberts <klr3of8@gmail.com>
2019-01-22 11:15:09 -0700

    changed nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
