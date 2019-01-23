2a8cba6ded017c0a268d61f58371ef010b9700de
Kayla Roberts <klr3of8@gmail.com>
2019-01-23 13:10:54 -0700

    puppies page

--- web/wiw/index.php
+++ web/wiw/index.php
@@ -27,7 +27,7 @@ switch ($action) {
         foreach ($puppies as $puppy) {
             $puppyGrid .= '<div class="gridItem">
                     <img src="' . $puppy['ImagePath'] . '" alt="' . $puppy['ImageDescription'] . '">
-                    <p>' . $puppy['Name'] . '</p>
+                    <h4>' . $puppy['Name'] . '</h4>
                     <p>Price: ' . $puppy['Price'] . '</p>
                     <a class="btn btn-warning" href="index.php?action=addCart&id='. $puppy['id'] .'">Add to Cart</a>
                 </div>';
