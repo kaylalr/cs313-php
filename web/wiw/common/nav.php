4058912ea32797e793a02aa153152f65ec9538db
Kayla Roberts <klr3of8@gmail.com>
2019-01-22 17:22:24 -0700

    cart nav

--- web/wiw/common/nav.php
+++ web/wiw/common/nav.php
@@ -1,27 +1,24 @@
-<!--<ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a>Other</a></li>
-    <li><a>Link</a></li>
-</ul>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a>Other</a></li>
+        <li><a>Link</a></li>
+    </ul>
+</nav>
 <button type="button" class="btn btn-default btn-sm">
     <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
-</button>-->
+</button>
 
-<!--<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-      
-  </div>
-</nav>-->
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">    
-<!--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+<!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark">    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>-->
+    </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
             <a class="nav-item nav-link" href="index.php">Home</a>
             <a class="nav-item nav-link" href="">About</a>
-            <!--<a class="nav-item nav-link" href="">Login</a>-->
+            <a class="nav-item nav-link" href="">Login</a>
         </div>
     </div>
-</nav>
+</nav>-->
