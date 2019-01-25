77110b72fbd9f62806907c0a590780ca332eaa34
Kayla Roberts <klr3of8@gmail.com>
2019-01-24 17:12:03 -0700

    shopping cart

--- web/wiw/common/header.php
+++ web/wiw/common/header.php
@@ -1,10 +1,10 @@
 <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a>Other</a></li>
-        <li><a>Link</a></li>
-        <li><a>Another</a></li>
-        <li><a>Link</a></li>
+        <li><a href="#">Other</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Another</a></li>
+        <li><a href="#">Link</a></li>
     </ul>
 </nav>
 <button type="button" class="btn btn-default btn-sm"><a href="index.php?action=cart">
