296c5f1ca670b15abb481570120ee1ea6d844a08
Kayla Roberts <klr3of8@gmail.com>
2019-01-26 20:18:27 -0700

    shopping cart

--- web/wiw/puppies.php
+++ web/wiw/puppies.php
@@ -21,6 +21,7 @@
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                 </select>
+                <button type="submit">Go</button>
             </form>
             <?php echo $puppyGrid; ?>
         </main>
