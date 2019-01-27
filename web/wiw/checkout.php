6ab6801a6a321cb619936bae50f97de9327a556b
Kayla Roberts <klr3of8@gmail.com>
2019-01-26 17:38:33 -0700

    shopping cart

--- web/wiw/checkout.php
+++ web/wiw/checkout.php
@@ -33,36 +33,37 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="inputFirstName">First Name:</label>
-                        <input type="text" class="form-control" id="inputFirstName" placeholder="First Name">
+                        <input type="text" class="form-control" name="inputFirstName" placeholder="First Name">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="inputLastName">Last Name:</label>
-                        <input type="password" class="form-control" id="inputLastName" placeholder="Last Name">
+                        <input type="password" class="form-control" name="inputLastName" placeholder="Last Name">
                     </div>
                 </div>
                 <div class="form-group formPadding">
                     <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    <input type="text" class="form-control" name="inputAddress" placeholder="1234 Main St">
                 </div>
                 <div class="form-group formPadding">
                     <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    <input type="text" class="form-control" name="inputAddress2" placeholder="Apartment, studio, or floor">
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
+                        <input type="text" class="form-control" name="inputCity">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
+<!--                        <select name="inputState" class="form-control">
                             <option selected>Choose...</option>
                             <option>...</option>
-                        </select>
+                        </select>-->
+                        <input type="text" class="form-control" name="inputState">
                     </div>
                     <div class="form-group col-md-2">
                         <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
+                        <input type="text" class="form-control" name="inputZip">
                     </div>
                 </div>
                 <div class="checkoutBtnDiv formPadding">
