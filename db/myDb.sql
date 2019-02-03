d963f1d1d90431fae9424837479c73434dc999c6
Kayla Roberts <klr3of8@gmail.com>
2019-02-02 21:07:17 -0700

    sql file

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,40 +1,36 @@
-CREATE TABLE MommyDogs (
-    mommyDogId      int             NOT NULL,
-    name            nvarchar(50)    NOT NULL,
-    PRIMARY KEY (mommyDogId)
+-- a female mother dog is called a dam.  This is a table to hold the mother dogs, so that is why the table is called Dams.
+CREATE TABLE Dams (
+    damId           int             NOT NULL,
+    name            varchar(50)    NOT NULL,
+    description     text            NOT NULL,
+    PRIMARY KEY (damId)
 );
 
 CREATE TABLE Puppies (
     puppyId         int             NOT NULL,
-    mommyDogId      int             NOT NULL,
-    name            nvarchar(50)    NOT NULL,
-    age             int             NOT NULL,
-    price           int             NOT NULL,
-    quality         ????            NOT NULL,
-    details         nvarchar(max)   NOT NULL,
-    /* add available or sold boolean */
-    PRIMARY KEY (puppyId)
-    FOREIGN KEY (mommyDogId) REFERENCES mommyDogs(mommyDogId)
+    DamId           int             NOT NULL,
+    name            varchar(50)    NOT NULL,
+    birthdate       date            NOT NULL,
+    details         text            NOT NULL,
+    sold            boolean,
+    PRIMARY KEY (puppyId),
+    FOREIGN KEY (damId) REFERENCES Dams(damId)
 );
 
 CREATE TABLE Images (
     imageId         int             NOT NULL,
-    imgPath         nvarchar(150)   NOT NULL,
-    imgDescription  nvarchar(100)   NOT NULL,
-    puppyId         int,    /* nullable because you might just want to add pictures for the gallery*/
-    mommyDogId      int,    /* nullable for same reason as puppyId */
-    /* add boolean fields?
-        this would allow you to have a checkbox and you can choose what the image is of,
-        this would help with the gallery - you could have a section for dogs, for puppies, and for other, maybe more?  */
+    imgPath         varchar(150)   NOT NULL,
+    imgDescription  varchar(100)   NOT NULL,
+    puppyId         int,    /* nullable because you might want to just add pictures for the gallery */
+    damId           int,    /* nullable for same reason as puppyId */
     PRIMARY KEY (imageId),
-    FOREIGN KEY (puppyId), REFERENCES Puppies(puppyId),
-    FOREIGN KEY (mommyDogId), REFERENCES MommyDogs(mommyDogId)
+    FOREIGN KEY (puppyId) REFERENCES Puppies(puppyId),
+    FOREIGN KEY (damId) REFERENCES Dams(damId)
 );
 
 CREATE TABLE users (
     userId          int             NOT NULL,
-    userName        nvarchar(50)    NOT NULL,
-    /* hash password */
-    userPassword    nvarchar(100)   NOT NULL,
+    userName        varchar(50)    NOT NULL,
+    userPassword    varchar(100)   NOT NULL,
     PRIMARY KEY (userId)
 );
