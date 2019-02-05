35fe63d2364da3cb3b387084d9ff8e13171405ec
Kayla Roberts <klr3of8@gmail.com>
2019-02-05 11:44:21 -0700

    database

--- web/wiw/model/puppies-model.php
+++ web/wiw/model/puppies-model.php
@@ -2,12 +2,25 @@
 
 function getAllPuppies() {
     $db = dbConnect();
-    echo "dbConnect() working";
-    exit;
-    $sql = "Select * from Puppies";
-    $stmt = $db->prepare($sql);
+    $sql = 'Select * from Puppies';
+    $stmt = $db->query($sql);
     $stmt->execute();
     $puppies = $stmt->fetchAll(PDO::FFETCH_ASSOC);
     $stmt->closeCursor();
     return $puppies;
 }
+
+
+//    $db = acmeConnect();
+//    $sql = 'SELECT clientId, clientFirstname, clientLastname, clientEmail, clientLevel, clientPassword 
+//         FROM clients
+//         WHERE clientEmail = :email';
+//    $stmt = $db->prepare($sql);
+//    $stmt->bindValue(':email', $clientEmail, PDO::PARAM_STR);
+//    $stmt->execute();
+//    $clientData = $stmt->fetch(PDO::FETCH_ASSOC);
+//    $stmt->closeCursor();
+//    return $clientData;
+
+//$statement = $db->query('SELECT username, password FROM note_user');
+//while ($row = $statement->fetch(PDO::FETCH_ASSOC))
