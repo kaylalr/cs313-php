4f32f0d88a6b1e12bc75768273a3a63a4522fa88
Kayla Roberts <klr3of8@gmail.com>
2019-02-05 11:30:40 -0700

    database

--- web/wiw/library/connections.php
+++ web/wiw/library/connections.php
@@ -4,6 +4,8 @@ function dbConnect() {
 //    $dsn = "dbname=d84du8cv721gks host=ec2-107-20-185-27.compute-1.amazonaws.com port=5432 user=tnyaaptynueduy password=5e9346c7d5f83a2c8a964f87f8d3bf16b1e57c0de6de25c422c3f8e9591a091a sslmode=require";
 //    $connect = new PDO($dsn);
 //    return $connect;
+    echo "dbConnect() function being called";
+    exit;
     try {
         $dbUrl = getenv('DATABASE_URL');
 
