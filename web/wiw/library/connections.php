451db193b960fc4ecdace96daf50005809e28520
Kayla Roberts <klr3of8@gmail.com>
2019-02-05 11:29:17 -0700

    database

--- web/wiw/library/connections.php
+++ web/wiw/library/connections.php
@@ -18,6 +18,8 @@ function dbConnect() {
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo $db;
+        exit;
         return $db;
     } catch (PDOException $ex) {
         echo 'Error!: ' . $ex->getMessage();
