1a3849d2b3666249120eb7ed302e13ba4bfff344
Kayla Roberts <klr3of8@gmail.com>
2019-02-06 13:46:39 -0700

    database

--- web/wiw/library/connections.php
+++ web/wiw/library/connections.php
@@ -6,8 +6,9 @@ function dbConnect() {
 //    return $connect;
     try {
         $dbUrl = getenv('DATABASE_URL');
+
         $dbOpts = parse_url($dbUrl);
-        
+
         $dbHost = $dbOpts["host"];
         $dbPort = $dbOpts["port"];
         $dbUser = $dbOpts["user"];
@@ -15,6 +16,7 @@ function dbConnect() {
         $dbName = ltrim($dbOpts["path"], '/');
 
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     } catch (PDOException $ex) {
