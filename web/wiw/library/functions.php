bab1f2316beed806ae7357dea3bfe1428f961111
Kayla Roberts <klr3of8@gmail.com>
2019-02-09 12:51:58 -0700

    view puppies

--- web/wiw/library/functions.php
+++ web/wiw/library/functions.php
@@ -4,10 +4,8 @@ function showPuppies($puppies) {
     $showPuppies = "<div class='gridContainer'>";
     foreach ($puppies as $puppy) {
     $showPuppies .= '<div class="gridItem">
-                    <img src="' . $puppy['ImagePath'] . '" alt="' . $puppy['ImageDescription'] . '">
-                    <h4>' . $puppy['Name'] . '</h4>
-                    <p>Price: ' . $puppy['Price'] . '</p>
-                    <a class="btn btn-warning" href="index.php?action=addCart&id=' . $puppy['id'] . '">Add to Cart</a>
+                    <img src="' . $puppy['imgpath'] . '" alt="' . $puppy['imgdescription'] . '">
+                    <h4>' . $puppy['name'] . '</h4>
                 </div>';
     }
     $showPuppies .= '</div>';
