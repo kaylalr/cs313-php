10d67cb34f8c3751585e6bdb1996557a63900d5c
Kayla Roberts <klr3of8@gmail.com>
2019-02-09 13:03:54 -0700

    view puppies

--- web/wiw/library/functions.php
+++ web/wiw/library/functions.php
@@ -6,9 +6,13 @@ function showPuppies($puppies) {
     $showPuppies .= '<div class="gridItem">';
     if ($puppy['imgpath']) {
         $showPuppies .= '<img src="' . $puppy['imgpath'] . '" alt="' . $puppy['imgdescription'] . '">';
+    } else {
+        $showPuppies .= '<img src="/web/images/no-image.png" alt="no image available">';
     }
     $showPuppies = '<h4>' . $puppy['name'] . '</h4></div>';
     }
     $showPuppies .= '</div>';
+    echo $showPuppies;
+    exit;
     return $showPuppies;
 }
