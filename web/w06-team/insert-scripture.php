aa67ab15b65099ea4680df1440b83beabcca01b5
Kayla Roberts <klr3of8@gmail.com>
2019-02-12 11:15:57 -0700

    week 6 team assignment

--- web/w06-team/insert-scripture.php
+++ web/w06-team/insert-scripture.php
@@ -49,6 +49,8 @@ $db = dbConnect();
     // exit;
 
     foreach ($topics as $topic) {
+        echo $topic;
+        exit;
         $statement = $db->prepare('INSERT INTO scripture_topic VALUES (default, :scriptureid, :topicid)');
         $statement->bindValue(':scriptureid', $newScriptureId, PDO::PARAM_STR);
         $statement->bindValue(':topicid', $topic, PDO::PARAM_STR);
