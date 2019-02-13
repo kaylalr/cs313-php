dfa7238da756f900dabccb6203ff8f407a300c78
Kayla Roberts <klr3of8@gmail.com>
2019-02-13 11:44:58 -0700

    admin page

--- web/wiw/common/admin-sidenav.php
+++ web/wiw/common/admin-sidenav.php
@@ -3,10 +3,13 @@
     <h2>Puppies</h2>
     <a href="index.php?action=updatePuppies">Update Puppies</a><br>
     <a href="index.php?action=addPuppy">Add Puppy</a><br>
+    <a href="index.php?action=deletePuppies">Delete Puppy</a><br>
     <h2>Terriers</h2>
     <a href="index.php?action=updateTerriers">Update Terriers</a><br>
     <a href="index.php?action=addTerrier">Add Terrier</a><br>
+    <a href="index.php?action=deleteTerriers">Delete Terrier</a><br>
     <h2>Images</h2>
+    <a href="index.php?action=updateImage">Update Image</a><br>
     <a href="index.php?action=addImage">Add Image</a><br>
     <a href="index.php?action=deleteImage">Delete Image</a><br>
 </div>
