97f4ce1ecb9c8b5f0bf29d8fb957871aa0746139
Kayla Roberts <klr3of8@gmail.com>
2019-02-12 22:47:57 -0700

    update database

--- web/wiw/common/header.php
+++ web/wiw/common/header.php
@@ -5,11 +5,18 @@
         <li><a href="index.php?action=viewTerriers">Terriers</a></li>
         <li><a href="index.php?action=viewGallery">Gallery</a></li>
         <!--<li><a href="index.php?action=puppies">Puppies-Cart</a></li>-->
-<!--        <li><a href="#">Link</a></li>
-        <li><a href="#">Another</a></li>
-        <li><a href="#">Link</a></li>-->
+        <!--        <li><a href="#">Link</a></li>
+                <li><a href="#">Another</a></li>
+                <li><a href="#">Link</a></li>-->
     </ul>
 </nav>
+<?php
+if (isset($_SESSION['loggedin'])) {
+    if ($_SESSION['loggedin']) {
+        echo '<button class = "btn btn-default btn-sm pull-right"><a href="index.php?action=admin">Admin</a></button>';
+    }
+}
+?>
 <!--<button type="button" class="btn btn-default btn-sm"><a href="index.php?action=cart">
         <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
     </a></button>-->
