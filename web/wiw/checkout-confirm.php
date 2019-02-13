dbbc66da2a845c222cd72c5dbd2bdc0cc967e15d
Kayla Roberts <klr3of8@gmail.com>
2019-02-13 11:19:04 -0700

    admin page

--- web/wiw/checkout-confirm.php
+++ web/wiw/checkout-confirm.php
@@ -7,7 +7,6 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="main.css" type="text/css">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <title>Checkout</title>
     </head>
     <body>
