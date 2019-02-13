5e5a58eaee6c47d7591d413d98379ce4563bc6fd
Kayla Roberts <klr3of8@gmail.com>
2019-02-12 23:09:47 -0700

    update database

--- web/wiw/admin.php
+++ web/wiw/admin.php
@@ -21,21 +21,43 @@ if (!$_SESSION['loggedin']) {
         </header>
         <main>
             <h1>Admin</h1>
-            <div>
-                <div>
-                    <h2>Puppies</h2>
-                    <a href="index.php?action=updatePuppies">Update Puppies</a><br>
-                    <a href="index.php?action=addPuppy">Add Puppy</a><br>
-                </div>
-                <div>
-                    <h2>Terriers</h2>
-                    <a href="index.php?action=updateTerriers">Update Terriers</a><br>
-                    <a href="index.php?action=addTerrier">Add Terrier</a><br>
-                </div>
-                <div>
-                    <h2>Images</h2>
-                    <a href="index.php?action=addImage">Add Image</a><br>
-                    <a href="index.php?action=deleteImage">Delete Image</a><br>
+            <!--            <div>
+                            <div>
+                                <h2>Puppies</h2>
+                                <a href="index.php?action=updatePuppies">Update Puppies</a><br>
+                                <a href="index.php?action=addPuppy">Add Puppy</a><br>
+                            </div>
+                            <div>
+                                <h2>Terriers</h2>
+                                <a href="index.php?action=updateTerriers">Update Terriers</a><br>
+                                <a href="index.php?action=addTerrier">Add Terrier</a><br>
+                            </div>
+                            <div>
+                                <h2>Images</h2>
+                                <a href="index.php?action=addImage">Add Image</a><br>
+                                <a href="index.php?action=deleteImage">Delete Image</a><br>
+                            </div>
+                        </div>-->
+            <div class="row">
+                <div class="col-sm-2">
+                    <nav class="nav-sidebar">
+                        <ul class="nav">
+                            <li class="active"><a href="javascript:;">Home</a></li>
+                            <li><a href="javascript:;">About</a></li>
+                            <li><a href="javascript:;">Products</a></li>
+                            <li>
+                                <a href="#" data-toggle="collapse" data-target="#submenu-1"><i class="fa fa-fw fa-search"></i> MENU 1 <i class="fa fa-fw fa-angle-down pull-right"></i></a>
+                                <ul id="submenu-1" class="collapse">
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.1</a></li>
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.2</a></li>
+                                    <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.3</a></li>
+                                </ul>
+                            </li>
+                            <!--                            <li><a href="javascript:;">FAQ</a></li>
+                                                        <li class="nav-divider"></li>
+                                                        <li><a href="javascript:;"><i class="glyphicon glyphicon-off"></i> Sign in</a></li>-->
+                        </ul>
+                    </nav>
                 </div>
             </div>
 
