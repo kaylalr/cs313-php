0ff5dc0d5fb5c62427a42f27892fbbdaaba9b38b
Kayla Roberts <klr3of8@gmail.com>
2019-02-12 22:40:42 -0700

    update database

--- web/wiw/cart.php
+++ web/wiw/cart.php
@@ -6,8 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="main.css" type="text/css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <title>Shopping Cart | West Idaho Westies</title>
     </head>
     <body>
--- web/wiw/update-terrier.php
+++ web/wiw/update-terrier.php
@@ -11,8 +11,8 @@ if (!$_SESSION['loggedin']) {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="main.css" type="text/css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <title>Update Terrier</title>
     </head>
     <body>
@@ -23,11 +23,11 @@ if (!$_SESSION['loggedin']) {
             <h1>Update Terrier</h1>
             <form method="post" action="index.php?action=updateCurrentTerrier">
                 <label>Name:</label><br>
-                <input name="name" type="text" value="<?php echo $terrier['name']?>" required><br>
+                <input name="name" type="text" value="<?php echo $terrier['name'] ?>" required><br>
                 <label>Description:</label><br>
-                <input name="details" type="textArea" value="<?php echo $terrier['description']?>" required><br>
+                <input name="details" type="textArea" value="<?php echo $terrier['description'] ?>" required><br>
                 <p class="warning">To add an image for the puppy, go to the "Update Images" page</p>
-                <input type="hidden" name="damid" value="<?php echo $terrier['damid']?>">
+                <input type="hidden" name="damid" value="<?php echo $terrier['damid'] ?>">
                 <input type="submit" value="Update">
             </form>
         </main>
