8e27bea47c912f1a35d772354f363985fd5e6ea5
Kayla Roberts <klr3of8@gmail.com>
2019-02-13 10:33:48 -0700

    update database

--- web/wiw/admin.php
+++ web/wiw/admin.php
@@ -14,9 +14,9 @@ if (!$_SESSION['loggedin']) {
         <link rel="stylesheet" href="main.css" type="text/css">
 
         
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<!--        <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>-->
         
         
         <title>Admin</title>
