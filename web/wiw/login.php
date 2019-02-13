e3ca1258308b196c0e26d8c160b8199466e2ee85
Kayla Roberts <klr3of8@gmail.com>
2019-02-12 21:40:36 -0700

    update database

--- web/wiw/login.php
+++ web/wiw/login.php
@@ -9,9 +9,9 @@
         <link rel="stylesheet" href="main.css" type="text/css">
 
         <!--     don't know if I need this    -->
-<!--        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <title>Login</title>
     </head>
     <body>
