70c483b652dd8b3b12d48c7c6991e9f8da3327bd
Kayla Roberts <klr3of8@gmail.com>
2019-02-13 10:38:09 -0700

    update database

--- web/wiw/admin.php
+++ web/wiw/admin.php
@@ -12,13 +12,6 @@ if (!$_SESSION['loggedin']) {
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="main.css" type="text/css">
-
-        
-<!--        <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>-->
-        
-        
         <title>Admin</title>
     </head>
     <body>
@@ -27,111 +20,23 @@ if (!$_SESSION['loggedin']) {
         </header>
         <main>
             <h1>Admin</h1>
-            <!--                        <div>
-                                        <div>
-                                            <h2>Puppies</h2>
-                                            <a href="index.php?action=updatePuppies">Update Puppies</a><br>
-                                            <a href="index.php?action=addPuppy">Add Puppy</a><br>
-                                        </div>
-                                        <div>
-                                            <h2>Terriers</h2>
-                                            <a href="index.php?action=updateTerriers">Update Terriers</a><br>
-                                            <a href="index.php?action=addTerrier">Add Terrier</a><br>
-                                        </div>
-                                        <div>
-                                            <h2>Images</h2>
-                                            <a href="index.php?action=addImage">Add Image</a><br>
-                                            <a href="index.php?action=deleteImage">Delete Image</a><br>
-                                        </div>
-                                    </div>-->
-
-
-
-
-            <nav class="navbar navbar-inverse sidebar" role="navigation">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Brand</a>
-                    </div>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
-                            <li ><a href="#">Profile<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
-                            <li ><a href="#">Messages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
-                            <li ><a href="#">Profile<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
-                            <li ><a href="#">Messages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
+            <div class="sidebar">
+                <div>
+                    <h2>Puppies</h2>
+                    <a href="index.php?action=updatePuppies">Update Puppies</a><br>
+                    <a href="index.php?action=addPuppy">Add Puppy</a><br>
                 </div>
-            </nav>
-
-
-
-
-
-
-
-
-
-
-
-            <!--            <div class="row">
-                            <div class="col-sm-2">
-                                <nav class="nav-sidebar">
-                                    <ul class="nav">
-                                        <li class="active"><a href="#">Home</a></li>
-                                        <li><a href="#">About</a></li>
-                                        <li><a href="#">Products</a></li>
-                                        <li>
-                                            <a href="#" data-toggle="collapse" data-target="#submenu-1"><i class="fa fa-fw fa-search"></i> MENU 1 <i class="fa fa-fw fa-angle-down pull-right"></i></a>
-                                            <ul id="submenu-1" class="collapse">
-                                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.1</a></li>
-                                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.2</a></li>
-                                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.3</a></li>
-                                            </ul>
-                                        </li>
-                                                                    <li><a href="javascript:;">FAQ</a></li>
-                                                                    <li class="nav-divider"></li>
-                                                                    <li><a href="javascript:;"><i class="glyphicon glyphicon-off"></i> Sign in</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>-->
-
-
+                <div>
+                    <h2>Terriers</h2>
+                    <a href="index.php?action=updateTerriers">Update Terriers</a><br>
+                    <a href="index.php?action=addTerrier">Add Terrier</a><br>
+                </div>
+                <div>
+                    <h2>Images</h2>
+                    <a href="index.php?action=addImage">Add Image</a><br>
+                    <a href="index.php?action=deleteImage">Delete Image</a><br>
+                </div>
+            </div>
         </main>
     </body>
 </html>
