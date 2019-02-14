8e3970afb7a07e462feffc04fc0751ea5e862b4d
Kayla Roberts <klr3of8@gmail.com>
2019-02-14 09:24:35 -0700

    team activity

--- web/w06-team/scripture-list.php
+++ web/w06-team/scripture-list.php
@@ -1,7 +1,7 @@
 <?php
 
 function getScriptures() {
-$db = dbConnect();
+    $db = dbConnect();
     $statement = $db->prepare('SELECT s.book, s.chapter, s.verse, s.content, t.name FROM scriptures s JOIN scripture_topic st ON s.scriptureid = st.scriptureid JOIN topics t ON t.topicid = st.topicid');
     $statement->execute();
     $topics = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -20,15 +20,15 @@ $db = dbConnect();
 <body>
 <h1>All Scriptures</h1>
     <?php 
-    $scriptures = getScriptures();
-    $currentScripture = null;
-    foreach ($scriptures as $scripture) {
-        if ($currentScripture != "$scripture['book'] . $scripture['chapter'] . $scripture['verse']") {
-        echo "<h2>" . $scripture['book'] . " " . $scripture['chapter']  . ":" . $scripture['verse'] . "</h2>";
-        $currentScripture = "$scripture['book'] . $scripture['chapter'] . $scripture['verse']";
-        }
-        echo "<span>". $scripture['name'] . "</span>";
-    }
+    // $scriptures = getScriptures();
+    // $currentScripture = null;
+    // foreach ($scriptures as $scripture) {
+    //     if ($currentScripture != "$scripture['book'] . $scripture['chapter'] . $scripture['verse']") {
+    //     echo "<h2>" . $scripture['book'] . " " . $scripture['chapter']  . ":" . $scripture['verse'] . "</h2>";
+    //     $currentScripture = "$scripture['book'] . $scripture['chapter'] . $scripture['verse']";
+    //     }
+    //     echo "<span>". $scripture['name'] . "</span>";
+    // }
     ?>
 </body>
 </html>
