583315b555e29fd5d4fd555930dc6ea1df2d0f3a
Kayla Roberts <klr3of8@gmail.com>
2019-02-14 09:17:10 -0700

    admin page

--- web/wiw/library/functions.php
+++ web/wiw/library/functions.php
@@ -3,13 +3,15 @@
 function showPuppies($puppies) {
     $showPuppies = "<div class='gridContainer'>";
     foreach ($puppies as $puppy) {
-        $showPuppies .= '<div class="gridItem">';
-        if ($puppy['imgpath']) {
-            $showPuppies .= '<img src="' . $puppy['imgpath'] . '" alt="' . $puppy['imgdescription'] . '">';
-        } else {
-            $showPuppies .= '<img id="no-image" src="/wiw/images/no-image.png" alt="no image available">';
+        if ($puppy['name'] != null) {
+            $showPuppies .= '<div class="gridItem">';
+            if ($puppy['imgpath']) {
+                $showPuppies .= '<img src="' . $puppy['imgpath'] . '" alt="' . $puppy['imgdescription'] . '">';
+            } else {
+                $showPuppies .= '<img id="no-image" src="/wiw/images/no-image.png" alt="no image available">';
+            }
+            $showPuppies .= '<h4>' . $puppy['name'] . '</h4></div>';
         }
-        $showPuppies .= '<h4>' . $puppy['name'] . '</h4></div>';
     }
     $showPuppies .= '</div>';
     return $showPuppies;
